db4e0e3aaae0763eabb6ea82439b22ede7589d1a
Igor 🦀 <igorboiko4242@gmail.com>
2025-05-05 16:36:28 +0400

    build pipline finished

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ cp -r ./target/dx/igorrust/release/web/public/* ../Igor-4242.github.io/ &&
 
 cd ../Igor-4242.github.io/ &&
 
+echo igorrust.xyz > CNAME
+echo "Hosted on [igorrust.xyz](https://igorrust.xyz)" >> README.md
+
 git add . &&
 git commit -m "update" &&
 git push
